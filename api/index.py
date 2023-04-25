24a5e6af565d19f67365892939260e7a445fde36
IyasuH <eyasuhailegbr@gmail.com>
2023-04-25 23:09:49 +0300

    updates

--- api/index.py
+++ api/index.py
@@ -4,14 +4,19 @@ from dotenv import load_dotenv
 import logging
 from typing import Optional
 from fastapi import FastAPI
+import telegram
 from pydantic import BaseModel
 from telegram import Update, Bot
 from telegram.ext import CommandHandler, MessageHandler, Updater, Filters, Dispatcher, CallbackContext
+from deta import Deta
 
 load_dotenv()
 
 # TOKEN = os.environ.get("TELE_TOKEN")
 TOKEN = os.getenv("TELE_TOKEN")
+DETA_KEY = os.getenv("DETA_KEY")
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(message)s", level=logging.INFO)
 
 FIRST_MSG = """Hello {name}
 
@@ -26,6 +31,11 @@ You can join our to follow up for more discounts
 
 
 app = FastAPI()
+
+deta = Deta(DETA_KEY)
+
+customer_db = deta.Base
+
 
 class TelegramWebhook(BaseModel):
     update_id: int
@@ -44,10 +54,29 @@ class TelegramWebhook(BaseModel):
 
 def start(update: Update, context: CallbackContext):
     user = update.effective_user or update.effective_chat
-    name = getattr(user, "username", '')
-    discount_num = str(hash(name))[10:]
-    update.message.reply_text(text=FIRST_MSG.format(name=name,discount_num=discount_num), parse_mode="markdown")
-    # context.bot.send_message(chat_id=update.effective_chat.id, text="Hello {} Now you will have a discounts!".format(update.message.username))
+
+    user_name = getattr(user, "username", '')
+    first_name = getattr(user, "first_name", '')
+
+    discount_num = str(hash(user_name))[10:]
+    update.message.reply_text(text=FIRST_MSG.format(name=first_name,discount_num=discount_num), parse_mode=telegram.ParseMode.HTML)
+
+    # before adding new data first lets check if it already exists
+    customer_query = customer_db.get(str(user.id))
+    if customer_query != None:
+        # save every thing about user
+        user_dict = user.to_dict()
+        user_dict['discount_num'] = discount_num
+        # discount_use(if user used his/her discount or not) and default value is False
+        user_dict['discount_use'] = 'False'
+        # use id as key
+        user_dict['key'] = str(user.id)
+
+        # save to db
+        # using put since insert uses more time
+        customer_db.put(user_dict)
+
+        # context.bot.send_message(chat_id=update.effective_chat.id, text="Hello {} Now you will have a discounts!".format(update.message.username))
 
 def register_handlers(dispatcher):
     # start_handler = CommandHandler('start', start)
